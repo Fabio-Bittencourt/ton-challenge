05d6f5880de19bbb20ace8e420a55e5468954905
Fabio Bittencourt <fabio@adimplere.com.br>
2021-11-08 16:15:41 -0300

    feat: databse configuration
    database and orm configuration thats allow use the database

---
+++ src/core/lib/db.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+const { logger } = require('./logger');
+const { isProd } = require('./service');
+const { production, development } = require('../../config/database');
+
+const env = isProd ? production : development;
+const client = new Sequelize(env);
+
+module.exports = {
+  open: async () => {
+    try {
+      await client.authenticate();
+      logger.info('::DB::Connect:: OPEN');
+    } catch (error) {
+      console.log('🚀 ~ file: db.js ~ line 13 ~ open: ~ error', error);
+      logger.error('::ERROR::', error);
+      throw error;
+    }
+  },
+  close: async () => {
+    try {
+      await client.close();
+      logger.info('::DB::Shut down::');
+    } catch (error) {
+      console.log('🚀 ~ file: db.js ~ line 26 ~ close: ~ error', error);
+      logger.error('::ERROR::', error);
+      throw error;
+    }
+  },
+};
