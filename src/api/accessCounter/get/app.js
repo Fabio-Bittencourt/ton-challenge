17c736e88ca46a9db753786346dd9f4d5bda9de2
Fabio Bittencourt <fabio@adimplere.com.br>
2021-11-09 15:00:32 -0300

    feat: get counter visits

---
+++ src/api/accessCounter/get/app.js
@@ -0,0 +1,22 @@
+const { logger } = require('../../../core/lib/logger');
+const { connection } = require('../../../core/services/sequelize');
+const { get } = require('../controller');
+
+module.exports.handler = async (event) => {
+  const { url } = event.queryStringParameters;
+
+  logger.info('::Lambda Access Counter has been started::');
+  try {
+    connection();
+    const result = await get(url);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        result,
+      }),
+    };
+  } catch (error) {
+    logger.error('::Lambda Access Counter return a Error::', error);
+    throw error;
+  }
+};
